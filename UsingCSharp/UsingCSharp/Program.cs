e5a30dd0bf1fe594c4582a2cfe15ba07f4fa3756
Greg Oliver <golive@microsoft.com>
2018-07-12 16:25:41 +0100

    add the c# code

---
+++ UsingCSharp/UsingCSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ServiceBus;
+using System;
+
+namespace UsingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // example: https://namespace.servicebus.windows.net
+            string ehNamespace = "namespace";
+
+            // example: insights-telemetry-lad
+            string eventHubName = "insights-telemetry-lad";
+
+            // example: RootManageSharedAccessKey
+            string hubPolicyName = "RootManageSharedAccessKey";
+
+            // example: ZhLwp6lrVWQt5UFXXXXXXXXXXXXDuHWfY0J9RN1ctE=
+            string hubPolicyKey = "your-policy-key";
+
+            string resource = $"https://{ehNamespace}.servicebus.windows.net/{eventHubName}";
+
+            string token = SharedAccessSignatureTokenProvider.GetSharedAccessSignature(
+                hubPolicyName, hubPolicyKey,
+                resource, new TimeSpan(365, 0, 0, 0));
+
+            Console.WriteLine(token + "\r\n" + "Press any key");
+            Console.ReadKey();
+        }
+    }
+}
